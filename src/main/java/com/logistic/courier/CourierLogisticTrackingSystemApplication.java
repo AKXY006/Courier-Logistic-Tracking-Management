be2395867d95637d778807d4b1a52451393dfae8
AKXY006 <ak144336@gmail.com>
2026-07-08 17:34:48 +0530

    Initial commit

---
+++ src/main/java/com/logistic/courier/CourierLogisticTrackingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.logistic.courier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourierLogisticTrackingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourierLogisticTrackingSystemApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/logistic/courier/entity/Customer.java
@@ -0,0 +1,39 @@
+package com.logistic.courier.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="customer_id")
+	private Integer customerId;
+	
+	@Column(name="customer_name")
+	private String customerName;
+	
+	@Column(name="customer_email", unique = true, length = 25)
+	private String customerEmail;
+	
+	@Column(name="customer_phone_number", unique = true, length = 10)
+	private String customerPhoneNumber;
+	
+	@Column(name="customer_address", length = 300)
+	private String customerAddress;
+	
+	
+
+}
