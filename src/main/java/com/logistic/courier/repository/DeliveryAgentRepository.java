df75a3752014806b52cf7b848512598230d6489d
AKXY006 <ak144336@gmail.com>
2026-07-10 14:41:14 +0530

    complete

--- src/main/java/com/logistic/courier/repository/DeliveryAgentRepository.java
+++ src/main/java/com/logistic/courier/repository/DeliveryAgentRepository.java
@@ -1,10 +1,34 @@
 package com.logistic.courier.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 import com.logistic.courier.entity.DeliveryAgent;
 
 public interface DeliveryAgentRepository extends JpaRepository<DeliveryAgent, Integer>{
+	
+	    //1) Create DeliveryAgent -> PostMapping -> save()
+	
+		//2) Get All DeliveryAgent -> GetMapping -> findAll()
+
+		//3) Get By Id -> GetMapping -> getById()
+		
+		//4) Get By vehicleNumber -> GetMapping -> getByvehicleNumber()
+		Optional<DeliveryAgent> getByVehicleNumber(String vehicaleNumber);
+		
+		//5) Get By phoneNumber -> GetMapping -> getByphoneNumber()
+		Optional<DeliveryAgent> getByPhoneNumber(String phoneNumber);
+		
+		//6) Get By ratingGreaterThan -> GetMapping -> getByRatingGreaterThan()
+	    List<DeliveryAgent> getByRatingGreaterOptionalThan(String rating);
+		
+		//7) Update -> Patch/Put --> saveAll()
+		
+		//8) Delete -> DeleteMapping -> deleteById()
+		
+		//9) update Agent -> PatchMapping -> saveAll()
 
 
 }
--- src/main/java/com/logistic/courier/repository/PackageRepository.java
+++ src/main/java/com/logistic/courier/repository/PackageRepository.java
@@ -1,9 +1,21 @@
 package com.logistic.courier.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.logistic.courier.entity.Package;
+import com.logistic.courier.entity.PackageType;
+
 
 public interface PackageRepository extends JpaRepository<Package,Integer>{
+	
+	//1) Get All Package -> GetMapping -> findAll()
 
+	//2) Get By Id -> GetMapping -> getById()
+	
+	//3) Get By Type -> GetMapping -> getByType
+	List<Package> getBypackageType(PackageType packageType);
+
+	
 }
