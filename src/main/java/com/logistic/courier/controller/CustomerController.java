595566366c244aff3d23439fa5a4f5a4602125d4
AKXY006 <ak144336@gmail.com>
2026-07-12 10:13:14 +0530

    Complete

--- src/main/java/com/logistic/courier/controller/CustomerController.java
+++ src/main/java/com/logistic/courier/controller/CustomerController.java
@@ -1,9 +1,27 @@
 package com.logistic.courier.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.logistic.courier.entity.Customer;
+import com.logistic.courier.service.CustomerService;
+import com.logistic.courier.util.ResponseStructure;
+
 @RestController
+@RequestMapping("/customer")
 public class CustomerController {
+	
+	@Autowired
+	private CustomerService customerService;
+	
+	@PostMapping
+	public ResponseEntity<ResponseStructure<Customer>> save(@RequestBody Customer customer){
+		return customerService.saveCustomer(customer);
+	}
 
 	
 }
